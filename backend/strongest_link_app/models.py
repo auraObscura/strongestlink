441c43c91f1aa33eea11620dc62d57fe6cb77158
auraObscura <auraobscura@icloud.com>
2022-05-03 00:34:58 -0500

    add basic user, profile, and interaction models

--- backend/strongest_link_app/models.py
+++ backend/strongest_link_app/models.py
@@ -1,3 +1,24 @@
+from xml.dom import ValidationErr
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    friends = models.ManyToManyField("self", blank=True)
+    profile_img = models.ImageField(blank=True, upload_to="profiles")
+
+
+class FriendRequest(models.Model):
+    sender = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name="requests_sent"
+    )
+    receiver = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name="requests_received"
+    )
+    accepted = models.BooleanField(default=False)
+
+    def clean(self):
+        if self.sender == self.receiver:
+            raise ValidationErr("It wouldn't count, anyway")
+        return super().clean()
