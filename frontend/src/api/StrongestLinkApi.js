6e63cc2c943723ad8e550bd0b177fc553202fd0e
auraObscura <auraobscura@icloud.com>
2022-05-03 10:32:05 -0500

    merge internal api files

--- frontend/src/api/StrongestLinkApi.js
+++ frontend/src/api/StrongestLinkApi.js
@@ -32,5 +32,32 @@ StrongestLinkApi.refreshToken = async (refreshToken) => {
 };
 
 // internal model calls
+// methods for Posts
+StrongestLinkApi.postPost = async (postData) => {
+  return await apiHelpers.tryCatchFetch(
+    () => axios.post(`${BASE_URL}/posts/`, postData))
+}
+
+StrongestLinkApi.getAllPosts = async () => {
+  return await apiHelpers.tryCatchFetch(
+    () => axios.get(`${BASE_URL}/posts/`))
+}
+
+StrongestLinkApi.getPostByID = async (postID) => {
+  return await apiHelpers.tryCatchFetch(
+    () => axios.get(`${BASE_URL}/posts/${postID}`))
+}
+
+//methods for Comments 
+StrongestLinkApi.getCommentByID = async (commentID) => {
+  return await apiHelpers.tryCatchFetch(
+    () => axios.get(`${BASE_URL}/comments/${commentID}`))
+}
+
+StrongestLinkApi.postComment = async (commentData) => {
+  return await apiHelpers.tryCatchFetch(
+    () => axios.post(`${BASE_URL}/comments/`, commentData)
+  )
+}
 
 export default StrongestLinkApi;
