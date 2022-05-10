501dfa871f405960a1a689c383cce4849ced3110
katarzyna-kw <katarzyna.koch@gmail.com>
2022-05-10 12:36:35 -0500

    adding classes, small alignment fixes with css

--- frontend/src/components/Friends.js
+++ frontend/src/components/Friends.js
@@ -9,7 +9,7 @@ function Friends (props){
       &&
       props.friends.map(friend => (
         <div key={`friend#${friend.user.id}`} className="friend-container">
-          <img className="profile-avatar avatar-result" src={friend.profile_img}></img>
+          <img className="profile-avatar avatar-friend" src={friend.profile_img}></img>
           <a href={`#/user/${friend.user.id}`}><p>{friend.user.username}</p></a>
         </div>
         ))}
--- frontend/src/pages/Map/ProfileSmallImage.js
+++ frontend/src/pages/Map/ProfileSmallImage.js
@@ -1,7 +1,7 @@
 import '../../App.css'
 
 function ProfileSmallImage(props) {
-  return ( <img className="profile-avatar avatar-result" src={props.image} />);
+  return ( <img className="profile-avatar avatar-result avatar-list" src={props.image} />);
 }
 
 export default ProfileSmallImage;
